590202d726648e39f1cb749859b42e2480b7c06e
cak <cak@typeerror.com>
2025-06-13 04:56:45 -0400

    update output dir to `ht`

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import conference from "./conf.js";
 
 void (async () => {
   const fbDb = await firebaseInit();
-  const outputDir = "./out/defcon";
+  const outputDir = "./out/ht";
   const htConf = "DEFCON33";
 
   await conference(fbDb, htConf, outputDir);
